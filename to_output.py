9d6edb5c0059f754999715efe08c699691e8b8ea
emahon <esmahon11@gmail.com>
2015-11-29 10:39:32 -0700

    Script that takes a numpy array and puts it into the output form wanted by the competition.

---
+++ to_output.py
@@ -0,0 +1,14 @@
+"""
+Takes a 1D numpy array and the file name you want
+outputs a csv in the file format Kaggle wants
+Example here: https://www.kaggle.com/c/how-much-did-it-rain-ii/data
+Copy this into your directory so you can directly import it
+"""
+
+import csv
+
+def to_output(array, name):
+    outwriter = csv.writer(open(name,'w'),delimiter=",")
+    i = 1
+    for row in array:
+        outwriter.writerow([i,row])
