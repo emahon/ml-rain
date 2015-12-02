af079c4425c73629f7df035813ba23f24a9b37cb
Ryan Gooch <s.ryan.gooch@gmail.com>
2015-12-02 09:28:46 -0700

    updated stats

--- BasicAnalysis/stats.py
+++ BasicAnalysis/stats.py
@@ -35,6 +35,59 @@ def writetest(Xpreds, fil='testanswers.csv') :
 	for row in rows :
 		outwriter.writerow([row+1,Xpreds[row]])
 
+def plot_corr_matrix(X) :
+	"""
+	Plots correlation matrix for data
+	"""
+	import numpy as np
+	import pandas as pd
+	import seaborn as sns
+	import matplotlib.pyplot as plt
+
+	sns.set(style="white")
+
+	labels = ["radar dist",
+				"Ref",
+				"Ref 5x5 10th",
+				"Ref 5x5 50th",
+				"Ref 5x5 90th",
+				"RefComposite",
+				"RefComposite 5x5 10th",
+				"RefComposite 5x5 50th",
+				"RefComposite 5x5 90th",
+				"Rho_HV",
+				"Rho_HV 5x5 10th",
+				"Rho_HV 5x5 50th",
+				"Rho_HV 5x5 90th",
+				"Zdr",
+				"Zdr 5x5 10th",
+				"Zdr 5x5 50th",
+				"Zdr 5x5 90th",
+				"Kdp",
+				"Kdp 5x5 10th",
+				"Kdp 5x5 50th",
+				"Kdp 5x5 90th",
+				"Expected"]
+
+	d = pd.DataFrame(data=X[:,2:].copy(), columns=labels)
+
+	# Compute the correlation matrix
+	corr = d.corr()
+
+	# Set up the matplotlib figure
+	f, ax = plt.subplots(figsize=(11, 9))
+
+	sns.corrplot(d)
+	ax.set_title('Correlation Matrix for Radar Features and Output Variable')
+	ax.set_xlabel('Features (along diagonal)')
+	ax.set_ylabel('Correlation Values (upper triangle)')
+	f.tight_layout()
+	plt.show()
+
+	# sns.pairplot(d, hue="Quality")
+	# f.tight_layout()
+	# plt.show()
+
 if __name__ == '__main__':
 	# Get data
 	X, y = getdata('average.csv')
@@ -55,12 +108,17 @@ if __name__ == '__main__':
 
 	# Compile rows that have no nans for pure dataset
 	Xnonans = []
+	ynonans = []
 	for row in np.arange(0,X.shape[0]) :
 		if nonemissing(X[row]) == False :
 			Xnonans.append(X[row])
+			ynonans.append([y[row]])
 
 	# Array format
 	Xn = np.array(Xnonans)
-
+	yn = np.array(ynonans)
 	# Pure training set 
-	writetest(Xn,'nomissing.csv')
+	# writetest(Xn,'nomissing.csv')
+
+	# Plot Correlation Matrix
+	plot_corr_matrix(Xn)
