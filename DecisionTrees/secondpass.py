f1f51258d6e53bdd37559391851452bf6957b505
emahon <esmahon11@gmail.com>
2015-12-03 22:06:57 -0700

    So it turns out max of learning rate is 1. No wonder it was freaking out.

--- DecisionTrees/secondpass.py
+++ DecisionTrees/secondpass.py
@@ -51,9 +51,8 @@ Actual model to run
 def model(X,y):
     xg = xgboost.XGBRegressor()
     params = [{'max_depth':numpy.linspace(3,6,4).astype(int),
-        'learning_rate':numpy.logspace(-3,1,5),
-        'n_estimators':numpy.linspace(90,110,5).astype(int),
-        'nthread':[1]}]
+        'learning_rate':numpy.logspace(-3,0,4),
+        'n_estimators':numpy.linspace(90,110,5).astype(int)}]
     gridres = grid.GridSearchCV(estimator=xg,param_grid=params,cv=3)
     return gridres.fit(X,y)
 
